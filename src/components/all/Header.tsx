e42d6103bdf9213080ae10e78bcddf8ac14f2c7f
eunsu <songess@naver.com>
2024-06-23 17:44:39 +0900

    [FIX]: Header 수정 CEOS 클릭시 랜딩페이지로 이동

--- src/components/all/Header.tsx
+++ src/components/all/Header.tsx
@@ -1,9 +1,14 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+
 const DUMMYUSER = 'FE 홍길동'
 
 export function Header(){
+    const router = useRouter();
     return (
         <header className="w-full h-[80px] bg-white flex justify-between items-center px-[20px] absolute top-0 left-0">
-            <h1 className="text-[28px] text-themeColor font-bold">CEOS</h1>
+            <h1 className="text-[28px] text-themeColor font-bold cursor-pointer" onClick={() => {router.push('/')}}>CEOS</h1>
             <div className="flex gap-2 items-center">
               <span className='text-[16px]'>{DUMMYUSER}</span>
               <button className="bg-themeColor text-white	 text-[18px] rounded-[10px] border-none w-[80px] h-[40px]">로그인</button>
